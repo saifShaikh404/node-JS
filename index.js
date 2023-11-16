36b2cb79004a6caf0e1ff0c24e068e02c056671a
saifShaikh404 <132943658+saifShaikh404@users.noreply.github.com>
2023-11-17 01:37:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
-// Common JS import
-// let funcs = require('./export.js') 
-// console.log(funcs.sum(5,6), funcs.minus(6, 10))
-
-
-// Pakage Manager 
-// let fs = require("fs"); // internal pakage of node didnt need to install externally
-// console.log("started here")
-
-// This read file sychronusly which is bad for performance
-// let data = fs.readFileSync("./text.txt","utf-8")
-// console.log(data)
-
-// This is async method with a callback function
-// let data = fs.readFile("./text.txt","utf-8",(err, data) => {
-//     console.log(data)
+// 1). Basic server setup
+// let http = require('http')
+// let server = http.createServer((req, res) => {
+//     res.end("<h1>Hello</h1>")
 // })
+// server.listen(8080)
 
-console.log("ended here")
+// 2). headers
+// let http = require('http')
+// let someData = { name: "Saif"};
+// let server = http.createServer((req, res) => {
+    // res.setHeader('Content-Type','text/html')
+    // res.end("Hello")
 
+    // res.setHeader('Content-Type','application/json')
+    // res.end(JSON.stringify(someData))
+// })
+// server.listen(8080)
 
+// 3). Sending html file and url and SSR
+let http = require('http')
+let fs = require('fs')
+let html = fs.readFileSync('./index.html','utf-8')
+let data = fs.readFileSync('./data.json','utf-8')
+
+let server = http.createServer((req, res) => {
+    let requestUrl = req.url.split('/')
+    let originalData = JSON.parse(data)
+    if(requestUrl[1] == 'demo'){
+        res.setHeader('Content-Type','text/html')
+        res.end("<h1>Demo</h1>")
+    } 
+    else if(requestUrl[1] == 'product') {
+        let dataToSend = originalData.find((data) => data.id == requestUrl[2])
+        if(dataToSend) {
+            console.log(dataToSend)
+            let newHTML = html
+            let modifiedHTML = newHTML.replace("**title**",dataToSend.name)
+            .replace("**desc**",dataToSend.desc)
+            res.setHeader('Content-Type','text/html')
+            res.end(modifiedHTML)
+        } else {
+            res.setHeader('Content-Type','text/html')
+            res.end("No Data Found")
+        }   
+    } 
+    else {
+        res.setHeader('Content-Type','text/html')
+        res.end(html)
+    }
+})
+server.listen(8080)
