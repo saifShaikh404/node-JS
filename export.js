63d7de201881b95eacd7e2c7c324073c6f128a87
saifShaikh404 <132943658+saifShaikh404@users.noreply.github.com>
2023-11-17 01:36:25 +0530

    Add files via upload

---
+++ export.js
@@ -0,0 +1,9 @@
+// Common JS module export
+
+exports.sum = (a, b) => {
+    return a + b
+}
+
+exports.minus = (a, b) => {
+    return a - b
+} 
---
+++ index.js
@@ -0,0 +1,21 @@
+// Common JS import
+// let funcs = require('./export.js') 
+// console.log(funcs.sum(5,6), funcs.minus(6, 10))
+
+
+// Pakage Manager 
+// let fs = require("fs"); // internal pakage of node didnt need to install externally
+// console.log("started here")
+
+// This read file sychronusly which is bad for performance
+// let data = fs.readFileSync("./text.txt","utf-8")
+// console.log(data)
+
+// This is async method with a callback function
+// let data = fs.readFile("./text.txt","utf-8",(err, data) => {
+//     console.log(data)
+// })
+
+console.log("ended here")
+
+
